f45d95d9698f05dbf91ef15929088b758b93a80f
bimal <mutainsbimal@gmail.com>
2023-09-01 08:27:04 +0545

    sep1

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -5,12 +5,23 @@ import { useState} from 'react';
 const Counter=()=>{
     const [counter,setCounter]=useState(0);
     function incrmeent(){
-        setCounter(counter+1);
+       
+            setCounter(counter+1);
+        
+    }
+    function decrement(){
+        if(counter <=0){
+
+        }else{
+            setCounter(counter-1);
+        }
+       
     }
 return(
     <div>
         <div>{counter}</div>
-        <button onClick={incrmeent}>+</button> <button>-</button>
+        <button onClick={incrmeent}>+</button>
+         <button onClick={decrement}>-</button>
     </div>
 )
 }
